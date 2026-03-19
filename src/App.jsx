10a9614d77923fe92116895a367438141613e535
illmedicine <demarkuswilsone@gmail.com>
2026-03-18 21:04:58 -0400

    feat: add persistent IRAI logo watermark across all pages

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,25 @@ function App() {
   window.showHome = showHome
 
   return (
-    <div className="bg-dark text-light min-h-screen">
-      <Navigation currentPage={currentPage} showCourse={showCourse} showHome={showHome} />
-      
-      {currentPage === 'home' && <HomePage showCourse={showCourse} />}
-      {currentPage === 'web-development' && <WebDevelopmentPage showHome={showHome} />}
-      {currentPage === 'mobile-app-builder' && <MobileAppBuilderPage showHome={showHome} />}
-      {currentPage === 'business-builder' && <BusinessBuilderPage showHome={showHome} />}
+    <div className="bg-dark text-light min-h-screen relative">
+      {/* Persistent IRAI Logo Watermark */}
+      <div className="fixed inset-0 flex items-center justify-center pointer-events-none z-0" aria-hidden="true">
+        <img 
+          src="./IRAILOGO.jpg"
+          alt=""
+          className="w-[600px] h-[600px] md:w-[700px] md:h-[700px] lg:w-[800px] lg:h-[800px] object-contain opacity-[0.04] select-none"
+          draggable="false"
+        />
+      </div>
+
+      <div className="relative z-10">
+        <Navigation currentPage={currentPage} showCourse={showCourse} showHome={showHome} />
+        
+        {currentPage === 'home' && <HomePage showCourse={showCourse} />}
+        {currentPage === 'web-development' && <WebDevelopmentPage showHome={showHome} />}
+        {currentPage === 'mobile-app-builder' && <MobileAppBuilderPage showHome={showHome} />}
+        {currentPage === 'business-builder' && <BusinessBuilderPage showHome={showHome} />}
+      </div>
     </div>
   )
 }

a584e41ca1fd34579e72246d748795ae90b5fbc1
bablx <babix0068@gmail.com>
2026-03-25 01:55:15 +0530

    Update base URL configuration for IRAI-Academy repository

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/IRAI">
+    <BrowserRouter basename="/IRAI-Academy">
       <App />
     </BrowserRouter>
   </StrictMode>,
